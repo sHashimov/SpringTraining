a351bbb7392974bde9e3b29817f27f4e93d49ff1
Sunay Hashimov <sunay.hashimov@mentormate.com>
2017-06-20 14:08:43 +0300

    added project

---
+++ gs-rest-service/initial/src/main/java/hello/Greeting.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Greeting {
+
+
+    private final String content;
+
+    public Greeting(String content) {
+
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ gs-rest-service/initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(String.format(template, name));
+    }
+}
